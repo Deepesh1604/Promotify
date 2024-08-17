83288e4f7054173d4d452355a49a5a5c9070121e
Deepesh1604 <deepeshdawar55@gmail.com>
2024-08-17 19:59:12 +0530

    improvement

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from sqlalchemy import func
 from sqlalchemy import or_
 import json
 
-app = Flask(__name__)
+app = Flask(__name__)#creta new flask instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # it set to false to improve performance since event isn't used in the application 
 app.config['SECRET_KEY'] = 'your_secret_key'  
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)#creates a SQLAlchemy object and binds it to your Flask application
 
 ADMIN_EMAIL = 'admin@gmail.com'
 ADMIN_PASSWORD = 'admin123'
